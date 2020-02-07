22ab32b06f7778513d474e9634a84ed931190631
ageronimo <jacobbc130@yahoo.com>
2020-02-07 10:37:55 -1000

    Tweaked HTML

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ function calculateTip() {
       alert("Please enter values");
       return;
     }
-
     if (numOfPeople === "" || numOfPeople <= 1) {
       numOfPeople = 1;
       document.getElementById("each").style.display = "none";
@@ -17,16 +16,11 @@ function calculateTip() {
       document.getElementById("each").style.display = "block";
     }
   
-
     var total = (billAmt * serviceQual) / numOfPeople;
-  
     total = Math.round(total * 100) / 100;
-   
     total = total.toFixed(2);
-   
     document.getElementById("totalTip").style.display = "block";
     document.getElementById("tip").innerHTML = total;
-  
   }
   
   document.getElementById("totalTip").style.display = "none";
@@ -34,5 +28,4 @@ function calculateTip() {
   
   document.getElementById("calculate").onclick = function() {
     calculateTip();
-  
   };
